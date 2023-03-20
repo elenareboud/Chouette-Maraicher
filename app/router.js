4c89191320892ce64389b95012337b52deabd45d
elenareboud <elena.reboud@gmail.com>
2023-03-20 10:53:25 +0100

    étapes 12, 13, 14

--- app/router.js
+++ app/router.js
@@ -1,23 +1,43 @@
 import express from 'express';
+import products from './data.js';
 
 const router = express.Router();
 
 
 
-router.get('/', function(req, res, next) {
-  console.log(`test`)
-    res.render('list',{
+router.get('/', function(req, res) {
+      res.render('list',{
       seoTitle: `Au Chouette Maraîcher`,
-    });
+      
+    });  
+    
   });
 
 router.get('/about', function(req, res) {
-    console.log(`test`)
+  
       res.render('about',{
       seoTitle: 'A propos',
       });
     });
 
+router.get('/:slug', function(req, res, next) {
+  const slug = req.params.slug;
+  console.log(slug);
+  const product = products.find(element => element.slug === slug);
+  
+
+    if(product){
+    res.render('product',{
+      seoTitle: `Au Chouette Maraîcher`,
+      title: product.title,
+      description: product.description,
+      
+    }); 
+  } 
+  else {
+    next();
+  }
+  });
 
 
 
